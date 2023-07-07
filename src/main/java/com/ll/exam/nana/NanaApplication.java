650b10bb53befa735905d443ba9fed9e771a1a15
kimdaeun12 <1004ekdms2@naver.com>
2023-07-07 09:20:30 +0900

    start

---
+++ src/main/java/com/ll/exam/nana/NanaApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.nana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/nana/NanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.nana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
